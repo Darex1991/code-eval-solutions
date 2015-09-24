eca709649fc3d7fcf8500a2bee71de167f4b8c70
Dariusz Pieńczykowski <dpienczykowski@gmail.com>
2015-09-24 22:25:20 +0200

    Another option

--- challenges/easy/reverse-words.js
+++ challenges/easy/reverse-words.js
@@ -11,3 +11,21 @@ fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) 
         process.stdout.write('\n');
     }
 });
+
+// Or
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line != "") {
+        array = line.split(' ');
+        leng = array.length;
+        secondLeng = leng;
+        
+        for(var i = 0 ; i<Math.floor((leng) /2); i++){
+            b = array[i];
+            array[i] = array[secondLeng-1];
+            array[secondLeng-1] = b;
+            secondLeng -= 1;
+        }
+        console.log(array.join(' '));
+    }
+});
